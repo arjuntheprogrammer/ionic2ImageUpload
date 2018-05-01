f434adaaa9c41340f9a02dcedf0e9de1905fdeee
arjun <saimzn4241@gmail.com>
2018-05-01 20:10:48 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,12 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+
+import { File } from '@ionic-native/file';
+import { Transfer } from '@ionic-native/transfer';
+import { FilePath } from '@ionic-native/file-path';
+import { Camera } from '@ionic-native/camera';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -23,6 +29,12 @@ import { HomePage } from '../pages/home/home';
   ],
   providers: [
     StatusBar,
+    
+    File,
+    Transfer,
+    Camera,
+    FilePath,
+
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
